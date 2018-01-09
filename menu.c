a2ea025a16a5a639eb341037ff90279b71391d8a
Cesar Hernandez <chernandezba@hotmail.com>
2018-01-09 20:46:13 +0100

    Fixed tzx browser on large files

--- menu.c
+++ menu.c
@@ -17273,7 +17273,13 @@ void menu_file_tzx_browser_show(char *filename)
 		int longitud_larga;
 		char buffer_bloque[256];
 
-
+		//printf ("ID %02XH puntero: %d indice_buffer: %d\n",tzx_id,puntero,indice_buffer);
+		//Aproximacion a llenado de buffer
+		if (indice_buffer>=MAX_TEXTO_BROWSER-1024) {
+			debug_printf(VERBOSE_ERR,"Too many entries. Showing only what is allowed on memory");
+			salir=1;
+			break;
+		}
 		//int subsalir;
 
 		switch (tzx_id) {
@@ -17302,11 +17308,11 @@ void menu_file_tzx_browser_show(char *filename)
 			case 0x11:
 
 				
-				sprintf(buffer_texto,"ID 11 - Turbo Sp. Data Block:");
+				sprintf(buffer_texto,"ID 11 - Turbo Data Block:");
 				indice_buffer +=util_add_string_newline(&texto_browser[indice_buffer],buffer_texto);
 
 				longitud_larga=tzx_file_mem[puntero+15]+256*tzx_file_mem[puntero+16]+65536*tzx_file_mem[puntero+17];
-				//printf("longitud larga: %d\n",longitud_larga);
+				printf("longitud larga: %d\n",longitud_larga);
 
 				//Longitud en este tipo de bloque es de 3 bytes, saltamos el primero para que la rutina
 				//de obtener cabecera de tap siga funcionando
