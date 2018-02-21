a8328cfe5076836621e96267ad36393c17deea62
chernandezba <chernandezba@hotmail.com>
2018-02-21 12:38:54 +0100

    mc

--- src/generate_readme_md.sh
+++ src/generate_readme_md.sh
@@ -32,6 +32,11 @@ cat << _EOF
 __Some screenshots__
 
 ![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_overscan.jpg "Overscan demo")
+![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_mazogs.png "Mazogs")
+![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_ql.png "QL")
+![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_z88.png "Z88")
+![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_sirfred_curses.png "Sirfred curses")
+![alt text](https://github.com/chernandezba/zesarux/raw/master/screenshots/screenshot_zxuno.png "ZX-Uno")
 
 You can find some ZEsarUX videos on my [Youtube channel](https://www.youtube.com/user/chernandezba)
 _EOF
